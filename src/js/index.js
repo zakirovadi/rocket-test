24f11b2b9cc53bb16146754f0887ddf85a901dd2
@Valera-username <valeriy.kr@rocketcompany.website>
2020-04-10 15:52:43 +0300

    ver 1.0.0

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+import $ from 'jquery'
+window.jQuery = $;
+window.$ = $;
+
+/*
+ON LOAD PAGE FUNCTION
+*/
+
+jQuery( window ).on( 'load', function() {
+
+    $('body').removeClass('is-load');
+
+} );
+
+/*
+INITIALIZATION FUNCTIONS
+*/
+
+jQuery( document ).ready( function( $ ) {
+
+
+
+} );
+
+/*
+ON SCROLL PAGE FUNCTIONS
+*/
+
+jQuery( window ).on( 'scroll', function() {
+
+
+
+} );
